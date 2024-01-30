bfe1ebd50ad8532db0121b9cdc86d74c4c738031
Christian Burau <Christian.burau@icloud.com>
2024-01-30 17:59:54 +0100

    Update

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! "$(docker ps -q --filter "name=yt-dlp")" ]; then
-    if [ -e /home/output/audio.txt ]; then
-        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output yt-dlp pwsh /home/ps/audio.ps1
+if [ ! "$(docker ps -q --filter "name=wiederda/yt-dlp")" ]; then
+    if [ -e /volume1/docker/audio.txt ]; then
+        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output wiederda/yt-dlp pwsh /home/ps/audio.ps1
     fi
 
-    if [ -e /home/output/video.txt ]; then
-        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output yt-dlp pwsh /home/ps/video.ps1
+    if [ -e /volume1/docker/video.txt ]; then
+        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output wiederda/yt-dlp pwsh /home/ps/video.ps1
     fi
 
-    if [ -e /home/output/playlist.txt ]; then
-        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output yt-dlp pwsh /home/ps/playlist.ps1
+    if [ -e /volume1/docker/playlist.txt ]; then
+        docker run -d --rm -m 1024m --name yt-dlp -e TZ=Europe/Berlin -v /volume1/docker/yt-dlp/:/home/output wiederda/yt-dlp pwsh /home/ps/playlist.ps1
     fi
 fi
